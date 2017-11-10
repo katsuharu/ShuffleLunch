bf5bd2920d10ec8343e3a4691e3f996cf6598216
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-10 20:30:48 +0900

    マッチング完了を知らせるメール用の関数を定義

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,13 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
+  def success_mail
+    user = User.find(1)
+    SuccessMailer.contact_contents(contact_params).deliver_now
+    flash[:success] = "マッチング成功メールの送信が完了しました。"
+    redirect_to root_url
+  end
+
   def send_mail
     ContactMailer.contact_contents(contact_params).deliver_now
     flash[:success] = "送信が完了しました。"
