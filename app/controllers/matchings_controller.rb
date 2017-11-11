5cd5445ba2be430b89d6b861ead6e7a048bcc710
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-11 18:27:00 +0900

    update文で、usersテーブルのカラム名に修正(pair_no→pair_id)

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -17,7 +17,7 @@ class MatchingsController < ApplicationController
 		if @@entry_id % 3 == 0
 			@@pair_no += 1
 			p @@pair_no
-			User.where(entry_id: @@entry_id-2 .. @@entry_id).update(pair_no: @@pair_no)
+			User.where(entry_id: @@entry_id-2 .. @@entry_id).update(pair_id: @@pair_no)
 
 	  		render action: 'show'
 	  	end
