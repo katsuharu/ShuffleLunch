45228ea05386b71c555c9a0ac50ce464d4d939e3
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-11 15:41:33 +0900

    クラスインスタンス変数を利用して値を保持。３の倍数ごとにpair_noの値を更新する。

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -2,17 +2,22 @@ class MatchingsController < ApplicationController
 	before_action :logged_in_user, :correct_user, only: [:index]
 	before_action :set_user, only: [:index] 
 	
-	max = 3
-	min = 2
+	@@entry_id = 0;
+	@@pair_no = 0;
 
   	def index
-
   	end
 
 	def entry
-		entry = Entry.new(user_id: current_user.id)
 		if entry.save
-		  flash[:success] = "シャッフルランチにエントリーしました。"
+			entry = Entry.new(user_id: current_user.id)
+			@@entry_id += 1;
+
+			if @@entry_id % 3 == 0
+				@@pair_no++;
+				Entry.where(entry_id: @@entry_id).update(pair_no: @@pair_no)
+
+		  	flash[:success] = "シャッフルランチにエントリーしました。"
 		else
 		  flash[:success] = "シャッフルランチにエントリーできませんでした。"
 		end
