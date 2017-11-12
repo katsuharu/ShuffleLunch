316b0ecd0b6ed1b6da71f0d8e79d1aa6241798ef
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-12 16:21:13 +0900

    フィルターにcorret_userを追加

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -1,6 +1,7 @@
 class MatchingsController < ApplicationController
 	before_action :logged_in_user, :correct_user, only: [:index, :entry, :check]
 	before_action :set_user, only: [:index, :entry, :check] 
+	before_action :correct_user,   only: [:index, :entry, :check]
 	
 	@@entry_id = 0
 	@@pair_no = 0
