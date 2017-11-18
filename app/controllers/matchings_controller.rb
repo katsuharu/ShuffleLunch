56acb25ff6462e91a0fa680985792693be3e1f2d
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-19 02:36:12 +0900

    renderを追加

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -14,6 +14,7 @@ class MatchingsController < ApplicationController
 		p @@entry_id
 		User.where(id: current_user.id).update(entry_id: @@entry_id)
 	  	flash[:success] = "シャッフルランチにエントリーしました。"
+	  	render action: 'waiting'
 
 		if @@entry_id % 3 == 0
 			@@pair_no += 1
